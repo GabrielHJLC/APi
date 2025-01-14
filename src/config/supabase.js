69ae4a45c17537ba7083ff5e6994798755bafb1a
GabrielHJLC <testegabriel483@gmail.com>
2025-01-14 12:44:25 -0300

    supabase

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,8 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,  // URL do seu projeto Supabase
-  process.env.SUPABASE_KEY   // A chave pública do seu projeto Supabase
+  process.env.SUPABASE_URL, 
+  process.env.SUPABASE_KEY,
+  {
+    auth: {
+      persistSession: false,
+    }
+  } 
 );
 
 export default supabase;
