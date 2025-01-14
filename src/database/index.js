928954d06c0abb3d023bb44ccea60b3820e9f821
GabrielHJLC <testegabriel483@gmail.com>
2025-01-14 01:00:39 -0300

    Database

--- src/database/index.js
+++ src/database/index.js
@@ -8,5 +8,16 @@ const models = [Aluno, User, Foto];
 
 const connection = new Sequelize(databaseConfig);
 
+connection.authenticate()
+    .then(() => {
+        console.log('Conexão com o banco de dados foi bem-sucedida!');
+    })
+    .catch((error) => {
+        console.error('Erro ao conectar ao banco de dados:', error);
+    });
+
+
 models.forEach((model) => model.init(connection));
 models.forEach((model) => model.associate && model.associate(connection.models));
+
+export default connection;
